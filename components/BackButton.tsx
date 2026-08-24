b61a3bf049e04215b474159a845f1fcf1467994a
David B <botezatu.david91@gmail.com>
2026-08-24 19:44:32 +0200

    Update the BackButton to use the new CustomMenuBar and IconLabelButton components.

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,23 +1,17 @@
+import CustomMenuBar from "@/components/CustomMenuBar";
+import IconLabelButton from "@/components/IconLabelButton";
 import {router} from "expo-router";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import {Pressable} from "react-native";
 
 export default function BackButton() {
     return (
-        <Pressable
-            accessibilityLabel="Back"
-            onPress={() => router.back()}
-            style={{
-                alignItems: "center",
-                marginHorizontal: 20,
-                marginBottom: 20,
-                paddingVertical: 10,
-                borderRadius: 999,
-                borderWidth: 1,
-                backgroundColor: "#e8e8e8"
-            }}
-        >
-            <Ionicons name="chevron-back" size={20} />
-        </Pressable>
+        <CustomMenuBar>
+            <IconLabelButton
+                label="Back"
+                iconName="return-up-back"
+                color="#8e8e93"
+                onPress={() => router.back()}
+                style={{ flex: 1 }}
+            />
+        </CustomMenuBar>
     );
 }
